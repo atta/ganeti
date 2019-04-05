a2ac4d79b06f34da753ad028b18cdd84ddb9f4e4
Apollon Oikonomopoulos <apoikos@dmesg.gr>
2019-04-05 17:46:39 +0300

    Adapt to Python 3 dict changes
    dict behavior has changed significantly in Python 3. According to PEP
    3106, dict.keys(), dict.values() and dict.items() have changed to
    provide a set-like, "live" object, instead of a static list. Their
    behavior resembles closely that of dict.iterkeys() et al from Python 2.
    
    In order for the code not to break, we need to make sure that all places
    relying on the result of the above methods being a list are adapted to
    explicitly request that. Luckily, most of the uses of .items() and
    .values() should have actually been .iteritems() and .itervalues(), so
    they can stay. There are a few cases that need to be updated though.
    
    While at it, drop explicit .keys() calls when iterating over dicts to
    make the code cleaner.
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@dmesg.gr>

--- lib/cmdlib/instance_query.py
+++ lib/cmdlib/instance_query.py
@@ -89,7 +89,7 @@ class LUInstanceQueryData(NoHooksLU):
         # via the node before it's locked, requiring verification later on
         self.needed_locks[locking.LEVEL_NODEGROUP] = \
           frozenset(group_uuid
-                    for instance_uuid in owned_instances.keys()
+                    for instance_uuid in owned_instances
                     for group_uuid in
                     self.cfg.GetInstanceNodeGroups(instance_uuid))
 
@@ -99,7 +99,7 @@ class LUInstanceQueryData(NoHooksLU):
       elif level == locking.LEVEL_NETWORK:
         self.needed_locks[locking.LEVEL_NETWORK] = \
           frozenset(net_uuid
-                    for instance_uuid in owned_instances.keys()
+                    for instance_uuid in owned_instances.keys
                     for net_uuid in
                     self.cfg.GetInstanceNetworks(instance_uuid))
 
@@ -127,7 +127,7 @@ class LUInstanceQueryData(NoHooksLU):
       assert not (owned_instances or owned_groups or
                   owned_node_uuids or owned_networks)
 
-    self.wanted_instances = instances.values()
+    self.wanted_instances = list(instances.values())
 
   def _ComputeBlockdevStatus(self, node_uuid, instance, dev):
     """Returns the status of a block device
